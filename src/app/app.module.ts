446e8c438b5f699b926357817097c680c79d22cc
sukhmeet1609 <sukhmeet1609@gmail.com>
2020-05-18 05:45:47 +0530

    lecture-50 Section 3, Course Project - The Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './components/server/server.component';
 import { ServersComponent } from './components/servers/servers.component';
+import { HeaderComponent } from './course-project/components/header/header.component';
+import { RecipesComponent } from './course-project/components/recipes/recipes.component';
+import { RecipeListComponent } from './course-project/components/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './course-project/components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './course-project/components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './course-project/components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './course-project/components/shopping-list/shopping-edit/shopping-edit.component';
 
 
 
@@ -12,7 +19,14 @@ import { ServersComponent } from './components/servers/servers.component';
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
